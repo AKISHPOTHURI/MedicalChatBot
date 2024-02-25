92a02a8985709d7782d801c710b7b986132fa980
AKISHPOTHURI <akishpothuri@gmail.com>
2024-02-25 16:42:38 +0530

    doing in jupyter

---
+++ constants/__init__.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+DATA_PATH = Path("C:\Users\akish\LangChain\MedicalChatBot\data\71763-gale-encyclopedia-of-medicine.-vol.-1.-2nd-ed.pdf")
+DB_FAISS_PATH =Path('vectorstore/db_faiss')
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'AKISH POTHURI',
+    author_email= 'akishpothuri@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
